7f3bcb224b6bbd01a4479f3878c6539b6bbb6a39
Alex Wade <wade@wadelab.net>
2014-01-21 15:20:20 +0000

    Editing analysis functions. Attempting to make this work on XPWin32 but think might have to give up due to memory limitations. Also tidied up display / plotting.

--- analyzeData/NewRig/fly_loadSSData.m
+++ analyzeData/NewRig/fly_loadSSData.m
@@ -3,7 +3,7 @@ function [phenotypeData,params]=fly_loadSSData(subDirList,extractionParams)
 %  Extracts data from files containing fly data. Returns summary data from
 % requested frequencies in phenotypeData and information about the
 % experiment (the params structure) in params.
-% This is a complete re-write of ss_loadFly.... 
+% This is a complete re-write of ss_loadFly....
 % Now works with new stimulus system, arbitrary mixing of background /
 % foreground contrasts etc.
 %
@@ -17,138 +17,139 @@ waveformSampleRate=extractionParams.waveformSampleRate; % Resample the average w
 %% Here we load in all the different data files.
 overallExptIndex=1;
 for thisSubDir=1:length(subDirList) % The concept of individual directories for different phenotypes is a little redundant now since all the ptype info is held in the file itself.
-                                          % And we can run multiple flies
-                                          % in a single session. However,
-                                          % we keep it as a concept because
-                                          % it help users organize their
-                                          % data.
-                                          
+    % And we can run multiple flies
+    % in a single session. However,
+    % we keep it as a concept because
+    % it help users organize their
+    % data.
     
     
-    % Go into each subdirectory and find out how many 
+    
+    % Go into each subdirectory and find out how many
     % many 'Fly...' or 'Expt...' subdirectories it contains.
-    
-    nExpts=length(subDirList{thisSubDir}.exptDir);
-    
+    if (isfield(subDirList{thisSubDir},'exptDir'))
+        nExpts=length(subDirList{thisSubDir}.exptDir);
+    else
+        disp('No expt directories found in this subDir');
+        nExpts=0;
+    end
     
     tic;
-    for thisExptIndex=1:nExpts
-        nDataSets=length(subDirList{thisSubDir}.exptData{thisExptIndex}.fileNames);
-        
-        condDat=[];
-        meanSpect=[];
-        
-        for thisFile=2:nDataSets % Load in all the files in the directory
+    if (nExpts>0) % Only go ahead if there are some sets to analyze...
+        for thisExptIndex=1:nExpts
+            nDataSets=length(subDirList{thisSubDir}.exptData{thisExptIndex}.fileNames);
             
-            % Here we can load in the data. I think we should do this for
-            % all file and post-process later.
-            fileName=fullfile(subDirList{thisSubDir}.exptDir{thisExptIndex},subDirList{thisSubDir}.exptData{thisExptIndex}.fileNames{thisFile});
-            disp(fileName);
-            
-            warning('off','all'); % Suppress Non-fatal warnings that are generated when you try to load DAQ toolbox objects without the DAQ TB installed.
-            allData=load(fileName);
-            warning('on','all');
-         
-            params=allData.exptParams; %  a struct containing experiment-relevent information
-            % We don't need it all - just the waveforms and some
-            % information about F1,F2, bins...
-            % allData.d is the most useful representation of the
-            % data. It is samples  x channels x
-            % contrastLevels x maskCondition
-            
-            nPreBins=params.nPreBins;
-            sampleRate=allData.digitizerSampleRate;
-            nTrials=size(allData.d,3); % How many probe and mask conditions in total
-           
-            nChannels=length(extractionParams.dataChannelIndices);% In general we have 2 input channels - 1 and 3. The middle channel here is the photodiode which is a useful check on input linearity.
-          
+            condDat=[];
+            meanSpect=[];
             
             
-            binsPerTrial=params.binsPerTrial;
-            rawPerFileData=reshape(squeeze(allData.d((sampleRate*nPreBins+1):end,extractionParams.dataChannelIndices,:)),[sampleRate,binsPerTrial,nChannels,nTrials]); % This gets rid of the first 'pre' bins...
             
-            condDat(:,:,:,:,thisFile)=rawPerFileData;
-            
-            % Sort the condDat - undo the randomization of contrast levels
+            % This is a place where you could add an option to ignore the first n
+            % datasets
+            for thisFile=1:nDataSets % Load in all the files in the directory
+                
+                % Here we can load in the data. I think we should do this for
+                % all files and post-process later.
+                fileName=fullfile(subDirList{thisSubDir}.exptDir{thisExptIndex},subDirList{thisSubDir}.exptData{thisExptIndex}.fileNames{thisFile});
+                disp(fileName);
+                
+                warning('off','all'); % Suppress Non-fatal warnings that are generated when you try to load DAQ toolbox objects without the DAQ TB installed.
+                allData=load(fileName);
+                warning('on','all');
+                
+                params=allData.exptParams; %  a struct containing experiment-relevent information
+                allOutData(overallExptIndex)=fly_parseGUIPhenotypeFields(params.b);
+                % We don't need it all - just the waveforms and some
+                % information about F1,F2, bins...
+                % allData.d is the most useful representation of the
+                % data. It is samples  x channels x
+                % contrastLevels x maskCondition
+                
+                nPreBins=params.nPreBins;
+                sampleRate=allData.digitizerSampleRate;
+                nTrials=size(allData.d,3); % How many probe and mask conditions in total
+                
+                nChannels=length(extractionParams.dataChannelIndices);% In general we have 2 input channels - 1 and 3. The middle channel here is the photodiode which is a useful check on input linearity
+                binsPerTrial=params.binsPerTrial;
+                rawPerFileData=reshape(squeeze(allData.d((sampleRate*nPreBins+1):end,extractionParams.dataChannelIndices,:)),[sampleRate,binsPerTrial,nChannels,nTrials]); % This gets rid of the first 'pre' bins...
+                % To get around memory issues and to just generally make things
+                % better, we average across bins straight away.
+                
+                % Cond dat at this point is sampleRate * nBins * nChannels * nConts * nFiles
+                % Make an average waveform for a single bin across all files, bins.
+                avWaveformThisFile=squeeze(nanmean(rawPerFileData,2)); % Average down bins
+                
+                
+                % [sampleRate,binsPerTrial,nChannels,nTrials, files]
+                % Sort the condDat - undo the randomization of contrast levels
                 [dummy,sortSeq]=sort(allData.exptParams.randSeq);
-                condDat(:,:,:,:,thisFile)=condDat(:,:,:,sortSeq,thisFile);
-
+                condDat(:,:,:,thisFile)=avWaveformThisFile(:,:,sortSeq);
+                
+                
+            end % Next file. Then we have a big set of bins
             
-        end % Next file. Then we have a big set of bins
-       
-        
-        allOutData(overallExptIndex)=fly_parseGUIPhenotypeFields(params.b);
-        
-        
             
-        % Cond dat at this point is sampleRate * nBins * nChannels * nConts * nFiles
-        % Make an average waveform for a single bin across all files, bins.
-        avWaveform=squeeze(nanmean(condDat,2)); % Average down bins
-        avWaveform=squeeze(nanmean(avWaveform,4)); % Average across files - all averaging is coherent.
-         
-        % Do some resampling to match the requested waveformSampleRate
-        currRate=size(avWaveform,1);
-       
+            
+            % Cond dat at this point is sampleRate * nBins * nChannels * nConts * nFiles
+            % Make an average waveform for a single bin across all files, bins.
+            
+            avWaveform=squeeze(nanmean(condDat,4)); % Average across files - all averaging is coherent.
+            
+            % Do some resampling to match the requested waveformSampleRate
+            currRate=size(avWaveform,1);
+            
+            
+            clear avWaveformRS;
+            if (currRate ~= waveformSampleRate)
+                avWaveformRS=resample(avWaveform,waveformSampleRate,currRate);
+            else
+                avWaveformRS=avWaveform;
+                
+            end
+            
+            %flyMeanWaveform{thisSubDir}.data(:,:,:,thisExptIndex)=avWaveformRS; % This now has both channels  in
+            
+            % We also want to do something else: Extract fourier domain
+            % components. We do this in two ways: Coherently (which is really
+            % just the FT of the time-average computed above). And incoherently
+            % where we want to discard phase information in each bin so that we preserve
+            % endogenous noise power.
+            
+            fCondDat=fft(condDat)/(size(condDat,1)); % Compute FT down time in each averaged bin. Leave in complex domain. Technically, we account for the length of the sample waveform although this is always the same...
+            
+            
+           % meanSpect=squeeze(nanmean(abs(fCondDat(2:nFreqs,:,:,:)),2));
+          % compMeanSpect=squeeze(nanmean((fCondDat(2:nFreqs,:,:,:)),2));
+            
+           % flyMeanSpectrum{thisSubDir}.data(:,:,:,thisExptIndex)=squeeze(nanmean(meanSpect,4));% Average across files for a single fly
+           % flyCompMeanSpectrum{thisSubDir}.data(:,:,:,thisExptIndex)=squeeze(nanmean(compMeanSpect,4)); % The same frequency domain average but with complex vals.
+            
+            
+            computedFreqsToExtract=abs(params.F(1).Freq*freqsToExtract(:,1)+params.F(2).Freq*freqsToExtract(:,2)); % Which frequencies are we going to extract
+            
+            params.extractedFreqs=computedFreqsToExtract;
+            
+            
+            
+            
+            fCondDat=fCondDat(computedFreqsToExtract+1,:,:,:); % Add 1 to the freqs to offset the mean baseline in the FFT
+            
+            meanDat=(squeeze(mean(fCondDat,4))); % was abs
+            
+            phenotypeData{thisSubDir}.expt{thisExptIndex}.allfCondDat=meanDat; % Average in the complex fourier domain across bins, then across repetitions
+            
+            
+            overallExptIndex=overallExptIndex+1;
+            
+        end % Next expt
         
-        clear avWaveformRS;
-        if (currRate ~= waveformSampleRate)
-            avWaveformRS=resample(avWaveform,waveformSampleRate,currRate);
-        else
-            avWaveformRS=avWaveform;
-             
-        end
+        %  phenotypeData{thisSubDir}.meanSpectrum=nanmean(flyMeanSpectrum{thisSubDir}.data,4);
+        % phenotypeData{thisSubDir}.semSpectrum=nanstd(flyMeanSpectrum{thisSubDir}.data,[],4)/sqrt(size(flyMeanSpectrum{thisSubDir}.data,4));
+        % phenotypeData{thisSubDir}.meanCompSpectrum=nanmean(flyCompMeanSpectrum{thisSubDir}.data,4);
+        %  phenotypeData{thisSubDir}.avWaveform=nanmean(flyMeanWaveform{thisSubDir}.data,4);
         
-        flyMeanWaveform{thisSubDir}.data(:,:,:,thisExptIndex)=avWaveformRS; % This now has both channels  in
-
-        % We also want to do something else: Extract fourier domain
-        % components. We do this in two ways: Coherently (which is really
-        % just the FT of the time-average computed above). And incoherently
-        % where we want to discard phase information in each bin so that we preserve
-        % endogenous noise power. 
-        
-        fCondDat=fft(condDat)/(size(condDat,1)); % Compute FT down time in each bin. Leave in complex domain. Technically, we account for the length of the sample waveform although this is always the same...
-        
-        
-        meanSpect=squeeze(nanmean(abs(fCondDat(2:nFreqs,:,:,:,:)),2));
-        compMeanSpect=squeeze(nanmean((fCondDat(2:nFreqs,:,:,:,:)),2));
-        
-        flyMeanSpectrum{thisSubDir}.data(:,:,:,thisExptIndex)=squeeze(nanmean(meanSpect,4));% Average across files for a single fly
-        flyCompMeanSpectrum{thisSubDir}.data(:,:,:,thisExptIndex)=squeeze(nanmean(compMeanSpect,4)); % The same frequency domain average but with complex vals.
-        
-         
-        
-        
-        
-        computedFreqsToExtract=abs(params.F(1).Freq*freqsToExtract(:,1)+params.F(2).Freq*freqsToExtract(:,2)); % Which frequencies are we going to extract
-        
-        params.extractedFreqs=computedFreqsToExtract;
-        
-     
-     
-        
-        fCondDat=(fCondDat(computedFreqsToExtract+1,:,:,:,:)); % Add 1 to the freqs to offset the mean baseline in the FFT
-       
+    end % End check on nExpt >0
     
-        
-     
-        meanDat=squeeze(mean(fCondDat,2));
-        meanDat=squeeze(mean(meanDat,4));
-          
-        
-        phenotypeData{thisSubDir}.expt{thisExptIndex}.allfCondDat=meanDat; % Average in the complex fourier domain across bins, then across repetitions
-      
-
-        overallExptIndex=overallExptIndex+1;
-
-    end % Next expt
     
-        phenotypeData{thisSubDir}.meanSpectrum=nanmean(flyMeanSpectrum{thisSubDir}.data,4);
-        phenotypeData{thisSubDir}.semSpectrum=nanstd(flyMeanSpectrum{thisSubDir}.data,[],4)/sqrt(size(flyMeanSpectrum{thisSubDir}.data,4));
-        phenotypeData{thisSubDir}.meanCompSpectrum=nanmean(flyCompMeanSpectrum{thisSubDir}.data,4);
-        phenotypeData{thisSubDir}.avWaveform=nanmean(flyMeanWaveform{thisSubDir}.data,4);
-        
-    
-
-    
-end
+end % Next subDir
 params.outType=allOutData;
