f10aad4a49e816b77875e893a8325ea598001e6c
wadelab <wade@wadelab.net>
2014-02-21 16:43:14 +0000

    Added functions to do hyperbolic ratio fitting (using matteobox)

---
+++ analyzeData/NewRig/fly_fitHyperData.m
@@ -0,0 +1,8 @@
+function [outParams]=fly_fitHyperData(params,contRange,dataToFit);
+% function fittedCRFParams=fly_fitHyperData(params,contRange,dataToFit)
+% Takes a data structure that is nPhenotypes x nPoints and fits a hyperbolic ratio to each phenotype. Returns Rmax, c50, exp, Rzero in fittedParams
+nPhenotypes=size(dataToFit,1);
+for thisPhenotype=1:nPhenotypes
+      [errFun(thisPhenotype),outParams(thisPhenotype,:)]=fit_hyper_ratioN2(squeeze(contRange(:)),squeeze(abs(dataToFit(thisPhenotype,:))),5,0,2);
+end
+    
