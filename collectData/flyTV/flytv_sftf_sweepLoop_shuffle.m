978b85bceed9ce1f165049d1f7038990f6d66b87
Alex Wade <wade@wadelab.net>
2014-04-15 11:27:06 +0100

    use long row rather than matrix for r

--- collectData/flyTV/flytv_sftf_sweepLoop_shuffle.m
+++ collectData/flyTV/flytv_sftf_sweepLoop_shuffle.m
@@ -6,29 +6,27 @@ sfList=[1 2 4 8 16]/1000; % We don't know what units this is in
 nTF=length(tfList);
 nSF=length(sfList);
 
-ordered=1:(nTF*nSF);
+ordered=1:(nTF*nSF); % This fully shuffles the order
 
-r=Shuffle(ordered); % Shuffle all the possible presentation conditions
+r=Shuffle(ordered); % Shuffle all the possible presentation conditions 
 
 
-%RandomTf = Shuffle(tfList)% shuffles tflist order
-%RandomSf = Shuffle (sfList)% shuffles sflist order
-
-for temporalFrequencyIndex=1:length(tfList)
-    for spatialFrequencyIndex=1:length(sfList)
-        thisaction= r(temporalFrequencyIndex,spatialFrequencyIndex)
-        t=ceil(thisaction/ nSF);
-        s=1+rem(thisaction , nTF);
-            thisTF=tfList(t)
-            thisSF=sfList(s)
+for thisrun=1:5 % 5 repeats
+    for temporalFrequencyIndex=1:nTF
+        for spatialFrequencyIndex=1:nSF
+            thisaction= r((temporalFrequencyIndex-1) * nTF + spatialFrequencyIndex)
+            t=ceil(thisaction/ nSF);
+            s=1+rem(thisaction, nTF); %  Should be 1+rem(thisAction-1,nTF)
+            thisTF=tfList(t);
+            thisSF=sfList(s);
             fprintf('\nRunning tf %d, sf %d',thisTF,thisSF);
             
-           
+            
             d=flytv_runGrating(thisTF,thisSF);     % This function runs the grating and acquires data. If we did the screen opening outside the loop (or made it static?)
-                                                    % We might not have to
-                                                    % open the screen each
-                                                    % time around.
-                                                    
+            % We might not have to
+            % open the screen each
+            % time around.
+            
             
             
             finalData.triggerTime=d.TriggerTime; % Extract the data into a new struct because DAQ objects don't save nicely
@@ -39,21 +37,23 @@ for temporalFrequencyIndex=1:length(tfList)
             finalData.comment='WT fly - data sweep';
             finalData.thisTF=thisTF;
             finalData.thisSF=thisSF;
+            finalData.thisTFIndex=t;
+            finalData.thisSFIndex=s;
             finalData.now=now;
             finalData.r=r;
-            
             
             dataSet{temporalFrequencyIndex,spatialFrequencyIndex}=finalData; % Put the extracted data into an array tf x sf
             
             
             
-    end % Next spatial frequency
-end % Next temporalFrequency
+        end % Next spatial frequency
+    end % Next temporalFrequency
+end    
 
 save temp % This should change to a uniquywe filename than embeds the time and date using datestr and now
 
 
 
 
-        
+
 
