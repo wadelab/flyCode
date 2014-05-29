4989f2f862144d0369cddd64a66440555260ad2c
alexwade <wade@wadelab.net>
2014-05-29 16:52:48 +0100

    Fiddling with analyse code to look at phase

--- analyzeData/flyTV/flytv_tempAnalyzeMask.m
+++ analyzeData/flyTV/flytv_tempAnalyzeMask.m
@@ -1,7 +1,7 @@
 %load('/Users/alexwade/Downloads/Sweep1 08.04.14.mat') % it is possible to browse files using uigetfile
 clear all
 close all;
-dataDir=uigetdir;
+dataDir=uigetdir
 fList=dir(fullfile(dataDir,'*.mat'))
 
 nFlies=length(fList);
@@ -35,7 +35,7 @@ for thisFly=1:nFlies
     [seq,ishuffleSeq]=sort(thisD.shuffleSeq);
     
     mGoodComps(thisFly,thisD.shuffleSeq,:)=(mean((goodComps(:,:,:))));
-    
+
     
     
     
@@ -43,13 +43,19 @@ for thisFly=1:nFlies
     
 end
 %%
-mvals=abs(squeeze(mean((mGoodComps))));
+pc1=1:7
+pc2=8:12;
+
+mvals=abs(squeeze(mean(abs(mGoodComps))));
+% allAng=angle(mGoodComps)-repmat(angle(mGoodComps(:,7,1)),[1 12 2]);
+% avals=squeeze(mean((allAng)));
+
 stVals=squeeze(std((mGoodComps)));
 semVals=stVals/sqrt(nFlies);
 
 figure(10);
-cLevels1=thisD.probeCont(1:6);
-cLevels2=thisD.probeCont(7:11);
+cLevels1=thisD.probeCont(pc1);
+cLevels2=thisD.probeCont(pc2);
 subplot(3,1,1);
 
 imagesc(abs(mGoodComps(:,:,1)));
@@ -58,9 +64,9 @@ subplot(3,1,2);
 hold off;
 
 
-h1=errorbar(cLevels1+.01,mvals(1:6),semVals(1:6),'k');
+h1=errorbar(cLevels1+.01,mvals(pc1),semVals(pc1),'k');
 hold on;
-h2=errorbar(cLevels2+.01,mvals(7:11),semVals(7:11),'r');set(gca,'XScale','Log');
+h2=errorbar(cLevels2+.01,mvals(pc2),semVals(pc2),'r');set(gca,'XScale','Log');
 set(h1,'LineWidth',2);
 set(h2,'LineWidth',2);
 
@@ -74,13 +80,25 @@ subplot(3,1,3);
 hold off;
 
 
-h1=errorbar(cLevels1+.01,mvals(1:6,2),semVals(1:6,2),'k');
+h1=errorbar(cLevels1+.01,mvals(pc1,2),semVals(pc1,2),'k');
 hold on;
-h2=errorbar(cLevels2+.01,mvals(7:11,2),semVals(7:11,2),'r');set(gca,'XScale','Log');
+h2=errorbar(cLevels2+.01,mvals(pc2,2),semVals(pc2,2),'r');set(gca,'XScale','Log');
 set(h1,'LineWidth',2);
 set(h2,'LineWidth',2);
 
 grid on;
-
-return;
+dataDir
+% 
+% figure(11);
+% hold off;
+% cLevels1=thisD.probeCont(pc1);
+% cLevels2=thisD.probeCont(pc2);
+% 
+% 
+% polar((180/pi)*(avals(pc1,1))',cLevels1+.01,'k');
+% hold on;
+% 
+% polar((180/pi)*(avals(pc2,1))',cLevels2+.01,'r');
+% hold off;
+% return;
 %%
