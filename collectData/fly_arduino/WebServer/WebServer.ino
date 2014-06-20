67cd2a7ae4b4a66c007c9f063098f277611d0d73
biol75 <biol75@york.ac.uk>
2014-06-20 13:09:25 +0100

    minor tidy up

--- collectData/fly_arduino/WebServer/WebServer.ino
+++ collectData/fly_arduino/WebServer/WebServer.ino
@@ -1,4 +1,4 @@
-#include <FixFFT.h>
+
 
 /*
 NB - take the SD card out for this to work !!!
@@ -25,6 +25,7 @@ NB - take the SD card out for this to work !!!
 #include <SPI.h>
 #include <Ethernet.h>
 #include <SD.h>
+//#include <FixFFT.h>
 
 const int max_graph_data = 32 ;
 int myGraphData [max_graph_data];
@@ -40,6 +41,7 @@ const float F2contrast[] = {
 int contrastOrder[ maxContrasts ]; 
 int iThisContrast = 0 ;
 
+
 int freq1 = 12 ; // flicker of LED Hz
 int freq2 = 15 ; // flicker of LED Hz
 // as of 18 June, maxdata of 2048 is too big for the mega....
@@ -51,6 +53,10 @@ long interval = 4;           // interval (5ms) at which to - 2 ms is also ok in 
 unsigned long last_time = 0; 
 unsigned long timing_too_fast = 0 ;
 
+
+#define N_WAVE          1024    /* dimension of Sinewave[] */
+#define LOG2_N_WAVE     10      /* log2(N_WAVE) */
+#define N_LOUD          100     /* dimension of Loudampl[] */
 
 
 const int MaxInputStr = 230 ;
@@ -93,41 +99,57 @@ void setup() {
   Serial.print("server is at ");
   Serial.println(Ethernet.localIP());
 
-doShuffle();
+  doShuffle();
 }
 
 void doShuffle()
 {
-for (int i = 0; i < maxContrasts; i++)
-{
-contrastOrder[i] = i ;
-}
+  for (int i = 0; i < maxContrasts; i++)
+  {
+    contrastOrder[i] = i ;
+  }
+  ///Knuth-Fisher-Yates shuffle algorithm.
+  randomSeed(analogRead(analogPin));
+  int randomnumber = random(maxContrasts);
+  int tmp ;
 
-///Knuth-Fisher-Yates shuffle algorithm.
-
-for (int i = maxContrasts - 1; i > 0; i--)
-{
-int n = random(i + 1);
-Swap(contrastOrder[i], contrastOrder[n]);
-}
+  for (int i = maxContrasts - 1; i > 0; i--)
+  {
+    int n = random(i + 1);
+    //Swap(contrastOrder[i], contrastOrder[n]);
+    tmp = contrastOrder[i];
+    contrastOrder[n] = contrastOrder[i];
+    contrastOrder[i] = tmp ;
+  }
 }
 
 void USE_SDCARD()
 {
+  pinMode(SS_SD_CARD, OUTPUT);
+  pinMode(SS_ETHERNET, OUTPUT);
   digitalWrite(SS_SD_CARD, LOW);  // HIGH means SD Card not active
   digitalWrite(SS_ETHERNET, HIGH); // HIGH means Ethernet not active
 }
 void USE_ETHERNET()
 {
+    pinMode(SS_SD_CARD, OUTPUT);
+  pinMode(SS_ETHERNET, OUTPUT);
   digitalWrite(SS_SD_CARD, HIGH);  // HIGH means SD Card not active
   digitalWrite(SS_ETHERNET, LOW); // HIGH means Ethernet not active
 }
 
-void sendHeader ()
+void sendHeader (bool isText = true)
 {
   // send a standard http response header
   client.println("HTTP/1.1 200 OK");
+  if (isText)
+  {
   client.println("Content-Type: text/html");
+  }
+  else
+  {
+    client.println("Content-Type: application/octet-stream");
+  }
   client.println("Connection: close");  // the connection will be closed after completion of the response
   client.println();
   client.println("<!DOCTYPE HTML>");
@@ -163,11 +185,21 @@ void serve_dir ()
 
 void run_graph()
 {
+
+  // read the value of  analog input pin and turn light on if in mid-stimulus...
+  int sensorReading = analogRead(analogPin);
+  myGraphData[iIndex] = sensorReading ;
+  iIndex ++ ;
+  if (iIndex > max_graph_data /10 && iIndex < max_graph_data/2)
+  {
+    analogWrite(ledPin, 255);
+  }
+  else
+  {
+    analogWrite(ledPin, 0);
+  }
+
   sendHeader () ;
-
-  client.println("<canvas id=\"myCanvas\" width=\"640\" height=\"520\" style=\"border:1px solid #d3d3d3;\">");
-  client.println("Your browser does not support the HTML5 canvas tag.</canvas>");
-
   client.println("<script>");
 
   // script to reload ...
@@ -179,57 +211,47 @@ void run_graph()
   client.println("function myStopFunction() {");
   client.println("clearInterval(myVar); }");
   client.println("");
+  client.println("</script>");
+// now do the graph...
+  client.println("<canvas id=\"myCanvas\" width=\"640\" height=\"520\" style=\"border:1px solid #d3d3d3;\">");
+  client.println("Your browser does not support the HTML5 canvas tag.</canvas>");
 
-
-
+  client.println("<script>");
   client.println("var c = document.getElementById(\"myCanvas\");");
   client.println("var ctx = c.getContext(\"2d\");");
-  // output the value of each analog input pin
-  for (int analogChannel = 1; analogChannel < 2; analogChannel++) {
-    int sensorReading = analogRead(analogChannel);
-    myGraphData[iIndex] = sensorReading ;
-    iIndex ++ ;
-    if (iIndex > max_graph_data /10 && iIndex < max_graph_data/2)
-    {
-      analogWrite(ledPin, 255);
-    }
-    else
-    {
-      analogWrite(ledPin, 0);
-    }
-    if (iIndex >= max_graph_data) iIndex = 0;
-    for (int i=0; i < max_graph_data-2; i++)
-    {
-      client.print("ctx.moveTo("); 
-      client.print(i*20); 
-      client.print(","); 
-      client.print(myGraphData[i]/2); 
-      client.println(");");
-      client.print("ctx.lineTo("); 
-      client.print((i+1)*20); 
-      client.print(","); 
-      client.print(myGraphData[i+1]/2); 
-      client.println(");");
-      client.println("ctx.stroke();");
-    }
-    //draw stimulus...
-    client.print("ctx.moveTo(");
-    client.print((max_graph_data /10)*20);
-    client.print(",");
-    client.print(30);
-    client.println(");");
 
-    client.print("ctx.lineTo(");
-    client.print(max_graph_data /2*20);
-    client.print(",");
-    client.print(30);
+  if (iIndex >= max_graph_data) iIndex = 0;
+  for (int i=0; i < max_graph_data-2; i++)
+  {
+    client.print("ctx.moveTo("); 
+    client.print(i*20); 
+    client.print(","); 
+    client.print(myGraphData[i]/2); 
     client.println(");");
-
-    client.println("ctx.strokeStyle=\"blue\";");
-    //              client.println("ctx.lineWidth=5;");
+    client.print("ctx.lineTo("); 
+    client.print((i+1)*20); 
+    client.print(","); 
+    client.print(myGraphData[i+1]/2); 
+    client.println(");");
     client.println("ctx.stroke();");
-
   }
+  //draw stimulus...
+  client.print("ctx.moveTo(");
+  client.print((max_graph_data /10)*20);
+  client.print(",");
+  client.print(30);
+  client.println(");");
+
+  client.print("ctx.lineTo(");
+  client.print(max_graph_data /2*20);
+  client.print(",");
+  client.print(30);
+  client.println(");");
+
+  client.println("ctx.strokeStyle=\"blue\";");
+  //              client.println("ctx.lineWidth=5;");
+  client.println("ctx.stroke();");
+
   client.println("</script>");
   client.println("<BR><BR><button onclick=\"myStopFunction()\">Stop display</button>");
   client.println("To run a flicker test please stop and then load <A HREF=\"http://biolpc22.york.ac.uk/cje2/form.html\"> form.html</A>  ");
@@ -273,17 +295,112 @@ void serve_dummy()
 
 int br_Now(double t)
 {
-int randomnumber = contrastOrder[iThisContrast];
+  int randomnumber = contrastOrder[iThisContrast];
   return int(sin((t/1000.0)*PI*2.0*double(freq1))*1.270 * F1contrast[randomnumber] + sin((t/1000.0)*PI*2.0*double(freq2))*1.270 * F1contrast[randomnumber])+127;
 }
 
 
+/** fix_fft() - perform fast Fourier transform. */
+int fix_fft(int fr[], int fi[], int m, int inverse) {
+  // fr[n],fi[n] are real,imaginary arrays, INPUT AND RESULT.
+  // size of data = 2**m
+  // set inverse to 0=dft, 1=idft
+
+  int mr, nn, i, j, l, k, istep, n, scale, shift;
+  int qr, qi, tr, ti, wr, wi;
+
+  n = 1<<m;
+
+  if(n > N_WAVE)
+    return -1;
+
+  mr = 0;
+  nn = n - 1;
+  scale = 0;
+
+  // decimation in time - re-order data
+  for(m=1; m<=nn; ++m) {
+    l = n;
+    do {
+      l >>= 1;
+    } 
+    while(mr+l > nn);
+    mr = (mr & (l-1)) + l;
+
+    if(mr <= m) continue;
+    tr = fr[m];
+    fr[m] = fr[mr];
+    fr[mr] = tr;
+    ti = fi[m];
+    fi[m] = fi[mr];
+    fi[mr] = ti;
+  }
+
+  l = 1;
+  k = LOG2_N_WAVE-1;
+  while(l < n) {
+    if(inverse) {
+      // variable scaling, depending upon data
+      shift = 0;
+      for(i=0; i<n; ++i) {
+        j = fr[i];
+        if(j < 0) j = -j;
+        m = fi[i];
+        if(m < 0) m = -m;
+        if(j > 16383 || m > 16383) {
+          shift = 1;
+          break;
+        }
+      }
+      if(shift)
+        ++scale;
+    }
+    else {
+      // fixed scaling, for proper normalization -
+      // there will be log2(n) passes, so this
+      // results in an overall factor of 1/n,
+      // distributed to maximize arithmetic accuracy.
+      shift = 1;
+    }
+    // it may not be obvious, but the shift will be performed
+    // on each data point exactly once, during this pass.
+    istep = l << 1;
+    for(m=0; m<l; ++m) {
+      j = m << k;
+      // 0 <= j < N_WAVE/2
+      wr =  getSinewave(j+N_WAVE/4);
+      wi = -getSinewave(j);
+      if(inverse) wi = -wi;
+      if(shift) {
+        wr >>= 1;
+        wi >>= 1;
+      }
+      for(i=m; i<n; i+=istep) {
+        j = i + l;
+        tr = fix_mpy(wr, fr[j]) - fix_mpy(wi, fi[j]);
+        ti = fix_mpy(wr, fi[j]) + fix_mpy(wi, fr[j]);
+        qr = fr[i];
+        qi = fi[i];
+        if(shift) {
+          qr >>= 1;
+          qi >>= 1;
+        }
+        fr[j] = qr - tr;
+        fi[j] = qi - ti;
+        fr[i] = qr + tr;
+        fi[i] = qi + ti;
+      }
+    }
+    --k;
+    l = istep;
+  }
+  return scale;
+}
 
 void collectData ()
 {
   const long presamples = 102;
   long mean = 0;
-
 
   sampleCount = -presamples ;
   while (sampleCount < max_data)
@@ -322,17 +439,28 @@ void collectData ()
   // now done with sampling....
   sampleCount ++ ;  
   analogWrite(ledPin, 127);
-  //writeFile("datalog.dat");
   iThisContrast ++;
-  if (iThisContrast > maxContrasts) iThisContrast = 0 ;
+  if (iThisContrast >= maxContrasts) iThisContrast = 0;
+  //writeFile("datalog.dat");
+  //destructive in place fft
+  int imag_in [max_data];
+  int real_in [max_data];
+  for (int i=0; i < max_data; i++)
+  {
+    imag_in [i] = 0;
+    real_in [i] = erg_in[i] ;
+  }
+  //  int iResult = fix_fft(real_in, imag_in, 10, 0 ); //2 ^ 10 1024
+
 }
 
 void flickerPage(String sCommand, bool RedoPage)
 {
   Serial.println ("Sampling at :" + String(sampleCount));
-  sendHeader();
+  
   if (!RedoPage) 
   {
+    sendHeader();
     client.println("<script>");
 
     // script to reload ...
@@ -350,7 +478,7 @@ void flickerPage(String sCommand, bool RedoPage)
   }
   else
   {
-     
+    sendHeader();// false);
     // retrieve the flicker rates we sampled with...
     int randomnumber = contrastOrder[iThisContrast];
     client.println("Data acquired at " + String(freq1) + " Hz with contrast " + String(int(F1contrast[randomnumber])) + 
@@ -459,9 +587,159 @@ void loop() {
 
 
 
+/* -------------------------------------------
+ **/
+#if N_WAVE != 1024
+ERROR: 
+N_WAVE != 1024
+#endif
+int PROGMEM Sinewave[1024] = {
+  0,    201,    402,    603,    804,   1005,   1206,   1406,
+  1607,   1808,   2009,   2209,   2410,   2610,   2811,   3011,
+  3211,   3411,   3611,   3811,   4011,   4210,   4409,   4608,
+  4807,   5006,   5205,   5403,   5601,   5799,   5997,   6195,
+  6392,   6589,   6786,   6982,   7179,   7375,   7571,   7766,
+  7961,   8156,   8351,   8545,   8739,   8932,   9126,   9319,
+  9511,   9703,   9895,  10087,  10278,  10469,  10659,  10849,
+  11038,  11227,  11416,  11604,  11792,  11980,  12166,  12353,
+  12539,  12724,  12909,  13094,  13278,  13462,  13645,  13827,
+  14009,  14191,  14372,  14552,  14732,  14911,  15090,  15268,
+  15446,  15623,  15799,  15975,  16150,  16325,  16499,  16672,
+  16845,  17017,  17189,  17360,  17530,  17699,  17868,  18036,
+  18204,  18371,  18537,  18702,  18867,  19031,  19194,  19357,
+  19519,  19680,  19840,  20000,  20159,  20317,  20474,  20631,
+  20787,  20942,  21096,  21249,  21402,  21554,  21705,  21855,
+  22004,  22153,  22301,  22448,  22594,  22739,  22883,  23027,
+  23169,  23311,  23452,  23592,  23731,  23869,  24006,  24143,
+  24278,  24413,  24546,  24679,  24811,  24942,  25072,  25201,
+  25329,  25456,  25582,  25707,  25831,  25954,  26077,  26198,
+  26318,  26437,  26556,  26673,  26789,  26905,  27019,  27132,
+  27244,  27355,  27466,  27575,  27683,  27790,  27896,  28001,
+  28105,  28208,  28309,  28410,  28510,  28608,  28706,  28802,
+  28897,  28992,  29085,  29177,  29268,  29358,  29446,  29534,
+  29621,  29706,  29790,  29873,  29955,  30036,  30116,  30195,
+  30272,  30349,  30424,  30498,  30571,  30643,  30713,  30783,
+  30851,  30918,  30984,  31049,
+  31113,  31175,  31236,  31297,
+  31356,  31413,  31470,  31525,  31580,  31633,  31684,  31735,
+  31785,  31833,  31880,  31926,  31970,  32014,  32056,  32097,
+  32137,  32176,  32213,  32249,  32284,  32318,  32350,  32382,
+  32412,  32441,  32468,  32495,  32520,  32544,  32567,  32588,
+  32609,  32628,  32646,  32662,  32678,  32692,  32705,  32717,
+  32727,  32736,  32744,  32751,  32757,  32761,  32764,  32766,
+  32767,  32766,  32764,  32761,  32757,  32751,  32744,  32736,
+  32727,  32717,  32705,  32692,  32678,  32662,  32646,  32628,
+  32609,  32588,  32567,  32544,  32520,  32495,  32468,  32441,
+  32412,  32382,  32350,  32318,  32284,  32249,  32213,  32176,
+  32137,  32097,  32056,  32014,  31970,  31926,  31880,  31833,
+  31785,  31735,  31684,  31633,  31580,  31525,  31470,  31413,
+  31356,  31297,  31236,  31175,  31113,  31049,  30984,  30918,
+  30851,  30783,  30713,  30643,  30571,  30498,  30424,  30349,
+  30272,  30195,  30116,  30036,  29955,  29873,  29790,  29706,
+  29621,  29534,  29446,  29358,  29268,  29177,  29085,  28992,
+  28897,  28802,  28706,  28608,  28510,  28410,  28309,  28208,
+  28105,  28001,  27896,  27790,  27683,  27575,  27466,  27355,
+  27244,  27132,  27019,  26905,  26789,  26673,  26556,  26437,
+  26318,  26198,  26077,  25954,  25831,  25707,  25582,  25456,
+  25329,  25201,  25072,  24942,  24811,  24679,  24546,  24413,
+  24278,  24143,  24006,  23869,  23731,  23592,  23452,  23311,
+  23169,  23027,  22883,  22739,  22594,  22448,  22301,  22153,
+  22004,  21855,  21705,  21554,  21402,  21249,  21096,  20942,
+  20787,  20631,  20474,  20317,  20159,  20000,  19840,  19680,
+  19519,  19357,  19194,  19031,  18867,  18702,  18537,  18371,
+  18204,  18036,  17868,  17699,  17530,  17360,  17189,  17017,
+  16845,  16672,  16499,  16325,  16150,  15975,  15799,  15623,
+  15446,  15268,  15090,  14911,  14732,  14552,  14372,  14191,
+  14009,  13827,  13645,  13462,  13278,  13094,  12909,  12724,
+  12539,  12353,  12166,  11980,  11792,  11604,  11416,  11227,
+  11038,  10849,  10659,  10469,  10278,  10087,   9895,   9703,
+  9511,   9319,   9126,   8932,   8739,   8545,   8351,   8156,
+  7961,   7766,   7571,   7375,   7179,   6982,   6786,   6589,
+  6392,   6195,   5997,   5799,   5601,   5403,   5205,   5006,
+  4807,   4608,   4409,   4210,   4011,   3811,   3611,   3411,
+  3211,   3011,   2811,   2610,   2410,   2209,   2009,   1808,
+  1607,   1406,   1206,   1005,    804,    603,    402,    201,
+  0,   -201,   -402,   -603,   -804,  -1005,  -1206,  -1406,
+  -1607,  -1808,  -2009,  -2209,  -2410,  -2610,  -2811,  -3011,
+  -3211,  -3411,  -3611,  -3811,  -4011,  -4210,  -4409,  -4608,
+  -4807,  -5006,  -5205,  -5403,  -5601,  -5799,  -5997,  -6195,
+  -6392,  -6589,  -6786,  -6982,  -7179,  -7375,  -7571,  -7766,
+  -7961,  -8156,  -8351,  -8545,  -8739,  -8932,  -9126,  -9319,
+  -9511,  -9703,  -9895, -10087, -10278, -10469, -10659, -10849,
+  -11038, -11227, -11416, -11604, -11792, -11980, -12166, -12353,
+  -12539, -12724, -12909, -13094, -13278, -13462, -13645, -13827,
+  -14009, -14191, -14372, -14552, -14732, -14911, -15090, -15268,
+  -15446, -15623, -15799, -15975, -16150, -16325, -16499, -16672,
+  -16845, -17017, -17189, -17360, -17530, -17699, -17868, -18036,
+  -18204, -18371, -18537, -18702, -18867, -19031, -19194, -19357,
+  -19519, -19680, -19840, -20000, -20159, -20317, -20474, -20631,
+  -20787, -20942, -21096, -21249, -21402, -21554, -21705, -21855,
+  -22004, -22153, -22301, -22448, -22594, -22739, -22883, -23027,
+  -23169, -23311, -23452, -23592, -23731, -23869, -24006, -24143,
+  -24278, -24413, -24546, -24679, -24811, -24942, -25072, -25201,
+  -25329, -25456, -25582, -25707, -25831, -25954, -26077, -26198,
+  -26318, -26437, -26556, -26673, -26789, -26905, -27019, -27132,
+  -27244, -27355, -27466, -27575, -27683, -27790, -27896, -28001,
+  -28105, -28208, -28309, -28410, -28510, -28608, -28706, -28802,
+  -28897, -28992, -29085, -29177, -29268, -29358, -29446, -29534,
+  -29621, -29706, -29790, -29873, -29955, -30036, -30116, -30195,
+  -30272, -30349, -30424, -30498, -30571, -30643, -30713, -30783,
+  -30851, -30918, -30984, -31049, -31113, -31175, -31236, -31297,
+  -31356, -31413, -31470, -31525, -31580, -31633, -31684, -31735,
+  -31785, -31833, -31880, -31926, -31970, -32014, -32056, -32097,
+  -32137, -32176, -32213, -32249, -32284, -32318, -32350, -32382,
+  -32412, -32441, -32468, -32495, -32520, -32544, -32567, -32588,
+  -32609, -32628, -32646, -32662, -32678, -32692, -32705, -32717,
+  -32727, -32736, -32744, -32751, -32757, -32761, -32764, -32766,
+  -32767, -32766, -32764, -32761, -32757, -32751, -32744, -32736,
+  -32727, -32717, -32705, -32692, -32678, -32662, -32646, -32628,
+  -32609, -32588, -32567, -32544, -32520, -32495, -32468, -32441,
+  -32412, -32382, -32350, -32318, -32284, -32249, -32213, -32176,
+  -32137, -32097, -32056, -32014, -31970, -31926, -31880, -31833,
+  -31785, -31735, -31684, -31633, -31580, -31525, -31470, -31413,
+  -31356, -31297, -31236, -31175, -31113, -31049, -30984, -30918,
+  -30851, -30783, -30713, -30643, -30571, -30498, -30424, -30349,
+  -30272, -30195, -30116, -30036, -29955, -29873, -29790, -29706,
+  -29621, -29534, -29446, -29358, -29268, -29177, -29085, -28992,
+  -28897, -28802, -28706, -28608, -28510, -28410, -28309, -28208,
+  -28105, -28001, -27896, -27790, -27683, -27575, -27466, -27355,
+  -27244, -27132, -27019, -26905, -26789, -26673, -26556, -26437,
+  -26318, -26198, -26077, -25954, -25831, -25707, -25582, -25456,
+  -25329, -25201, -25072, -24942, -24811, -24679, -24546, -24413,
+  -24278, -24143, -24006, -23869, -23731, -23592, -23452, -23311,
+  -23169, -23027, -22883, -22739, -22594, -22448, -22301, -22153,
+  -22004, -21855, -21705, -21554, -21402, -21249, -21096, -20942,
+  -20787, -20631, -20474, -20317, -20159, -20000, -19840, -19680,
+  -19519, -19357, -19194, -19031, -18867, -18702, -18537, -18371,
+  -18204, -18036, -17868, -17699, -17530, -17360, -17189, -17017,
+  -16845, -16672, -16499, -16325, -16150, -15975, -15799, -15623,
+  -15446, -15268, -15090, -14911, -14732, -14552, -14372, -14191,
+  -14009, -13827, -13645, -13462, -13278, -13094, -12909, -12724,
+  -12539, -12353, -12166, -11980, -11792, -11604, -11416, -11227,
+  -11038, -10849, -10659, -10469, -10278, -10087,  -9895,  -9703,
+  -9511,  -9319,  -9126,  -8932,  -8739,  -8545,  -8351,  -8156,
+  -7961,  -7766,  -7571,  -7375,  -7179,  -6982,  -6786,  -6589,
+  -6392,  -6195,  -5997,  -5799,  -5601,  -5403,  -5205,  -5006,
+  -4807,  -4608,  -4409,  -4210,  -4011,  -3811,  -3611,  -3411,
+  -3211,  -3011,  -2811,  -2610,  -2410,  -2209,  -2009,  -1808,
+  -1607,  -1406,  -1206,  -1005,   -804,   -603,   -402,   -201,
+};
 
 
 
+/** Return an integer indexed into the Sinewave table in flash */
+int getSinewave(int index) {
+  int val;
+
+  // Get the value from the flash memory
+  memcpy_P(&val, &Sinewave[index], sizeof(int));
+  return val;
+}
+
+/**  fix_mpy() - fixed-point multiplication */
+int fix_mpy(int a, int b) {
+  return ((long)(a) * (long)(b))>>15;
+}
 
 
 
