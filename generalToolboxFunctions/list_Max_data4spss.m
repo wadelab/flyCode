222d1cf5199beccfd999985af29df0c5cbf3af81
biol75 <biol75@york.ac.uk>
2014-08-19 10:20:14 +0100

    sort nasty printf

--- generalToolboxFunctions/list_Max_data4spss.m
+++ generalToolboxFunctions/list_Max_data4spss.m
@@ -32,53 +32,71 @@ end
 %% Now we have to write out the data in the format
 % Fly, F, contrast, mask, response, genotype 
 % we put genotype last so it can be split in Excel
-
+iPreviousFlies = 1;
 contrastmax = nConts;
 maskmax = 2 ;
 freqmax = nFreqs;
 genotypemax = length(analysisStruct.phenotypeName) ;
 
-fprintf (fileID,'fly, mask, freq, max response, max/masked F2[1], genotype \n');
+outcells(iPreviousFlies,:) = {'fly'; 'mask'; 'freq'; 'max response'; 'max/masked F2[1]'; 'genotype '};
+iPreviousFlies = iPreviousFlies + 1;
+
 for genotype = 1 : genotypemax
     flymax = analysisStruct.nFlies{genotype};
     for freq = 1 : freqmax
         for mask = 1 : maskmax
-            if ((( mask == 1 ) && ( freq == 2)) || (( mask == 1 ) && ( freq >= 4)) ) 
+            if ((( mask == 1 ) && ( freq == 2)) || (( mask == 1 ) && ( freq >= 4)) )
                 %% ignore the unmasked F and 2F
             else
-            
+                
                 for fly = 1 : flymax
-
+                    
                     max_response= 0.0;
-                    for contrast = 1 : contrastmax                    
-                    %calculate the response
-                    response = abs(analysisStruct.allFlyDataCoh{1,genotype}(fly,freq,contrast,mask));
-                    if (response > max_response)
-                        max_response = response ;
-                    end ;
-                    
-                    %%convert the indexes to humean friendly forms...
-                    genotype_str = analysisStruct.phenotypeName{genotype} ;
-                    if mask == 1
-                        mask_str='unmasked';
-                    else
-                        mask_str='masked';
-                    end
-                    
-                    freq_str= plotParams.labelList{freq} ;
+                    for contrast = 1 : contrastmax
+                        %calculate the response
+                        response = abs(analysisStruct.allFlyDataCoh{1,genotype}(fly,freq,contrast,mask));
+                        if (response > max_response)
+                            max_response = response ;
+                        end ;
+                        
+                        %%convert the indexes to humean friendly forms...
+                        genotype_str = analysisStruct.phenotypeName{genotype} ;
+                        if mask == 1
+                            mask_str='unmasked';
+                        else
+                            mask_str='masked';
+                        end
+                        
+                        freq_str= plotParams.labelList{freq} ;
                     end % contrast
                     
                     %%calculate the max_response as a ratio of the masked F2
                     F2_response = abs(analysisStruct.allFlyDataCoh{1,genotype}(fly,2,1,2));
                     ratio_response = max_response / F2_response ;
                     
-                    %%write the line
-                    fprintf (fileID, '%d,  %s, %s, %0.3e, %0.3e, %s \n', fly, mask_str, freq_str,  max_response, ratio_response, genotype_str);
+                    %%write the line         
+                    outcells(iPreviousFlies, :) = { num2str(fly); mask_str; freq_str;  num2str(max_response); num2str(ratio_response); genotype_str};
+                    iPreviousFlies = iPreviousFlies + 1;
                     
-                end %  fly
+                end
             end  %% if
-            end % freq
-        end % mask
+        end % freq
+    end % mask
     
 end % genotype
+
+%% well csvwrite doesn't like my cell matrix, so do it outselves
+%outM = celltomat(outcells);
+%csvwrite(outfile, outM);
+
+cSize = size(outcells);
+for i =1:cSize(1)
+    for j = 1: cSize(2)
+        fprintf (fileID, outcells{i,j});
+        fprintf (fileID, ', ');
+    end
+    fprintf (fileID,'\n');
+end
+        
+
 disp (['file written', outfile]);
