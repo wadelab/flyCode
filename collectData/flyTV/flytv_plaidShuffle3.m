f2bbfa7c393918929d20a1a2580d4a41ed6812f3
rw531 <ryan.west@york.ac.uk>
2014-11-10 17:00:52 +0000

    CVhanged

--- collectData/flyTV/flytv_plaidShuffle3.m
+++ collectData/flyTV/flytv_plaidShuffle3.m
@@ -23,7 +23,7 @@ if (~DUMMYRUN)
 end
 
 
-datadir='C:\data\SSERG\data\NewSweep\PINK1\1DPE\repoG2019S\';
+datadir='C:\data\SSERG\data\NewSweep\PINK1\1DPE\repoPINK1RNAi41671\';
 flyTV_startTime=now;
 
 
@@ -98,7 +98,7 @@ for thisRun=1:nRepeats  % 5 repeats
             finalData.flyName{2}='repo-G2019s_2_1dpe';
             
             
-            finalData.comment='1: repo-G2019s_1_1dpe 2: repo-G2019s_2_1dpe'; % Here : the first data channel ('ai0') is the bottom fly.
+            finalData.comment='1: repoPINK1RNAi41671_1DPE_3 2: repoPINK1RNAi41671_1DPE_4'; % Here : the first data channel ('ai0') is the bottom fly.
             finalData.stim=stim;
             finalData.now=now;
             finalData.nRepeats=nRepeats;
