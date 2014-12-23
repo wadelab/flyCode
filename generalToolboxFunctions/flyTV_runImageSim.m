3840a2257dbcb0e718669d647b86bc2635ec7e73
Alex Wade <wade@wadelab.net>
2014-12-23 11:46:41 +0000

    Tidied up

--- generalToolboxFunctions/flyTV_runImageSim.m
+++ generalToolboxFunctions/flyTV_runImageSim.m
@@ -3,34 +3,35 @@
 % This is about 2/3 of a fly's eye in each direction (which covers something like 110 degs)
 % So roughly we are simulating a square fov that is 110 *2/3 = 73 degrees
 % of visual angle each way
-
-
+% ARW 01/12/14 Wrote it - in github for flytv
 
 
 close all;
 clear all;
-load clown;
+load clown; % You can load in an image . Or make a grating yourself.
 X1=linspace(0,2*pi*20,256); % A .22 cpd grating has about 16 cycles in this FOV
 [xx,yy]=meshgrid(X1);
 i1=sin(xx);
-[simImage,sampPoints,origImage]=flyTV_imageSim(i1);
+
+% i1=X % This will use the clown image rather than the grating.
+
+[simImage,sampPoints,origImage]=flyTV_imageSim(i1); % Call the simulation function
 
 
 
 figure(2);
 
-
-
+% Do some plotting
 subplot(3,1,1);
 hold off;
 
-imagesc(origImage);
+imagesc(origImage); % Original image
 axis image;
 axis off;
 hold on;
 
 subplot(3,1,2);
-
+% Sampling
 h0=scatter(sampPoints.rX,sampPoints.rY,35,sampPoints.cols);
 
 set(h0,'MarkerFaceColor','flat');
@@ -46,6 +47,7 @@ axis off;
 % Now sample this using griddata
 
 subplot(3,1,3);
+% Sampled image
 colormap gray;
 
 
