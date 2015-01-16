8b4ae35042066c5090713ce74b7aa63c661b5e6a
Alex Wade <wade@wadelab.net>
2015-01-16 14:08:32 +0000

    Revert "Merge branch 'Stav_Sept14'"
    This reverts commit 53f41d7bb1dc89b7ca07fc65feb2b476a11c2106, reversing
    changes made to e3ff9933b992988b623832da2d7567784c829a71.

--- generalToolboxFunctions/plotfits.m
+++ generalToolboxFunctions/plotfits.m
@@ -18,56 +18,8 @@ load(fullfile(pathToLoad,fileToLoad));
 
 %% housekeeping 
 harmonicNames = {'1F1','1F2';'2F1','2F2'};
-%% list flies we find
-fliesWithRealData = 0;
-fittedNames = {};
-
-for thisPhenotype=1:length(analysisStruct.nFlies)
-    tmpName = analysisStruct.phenotypeName{thisPhenotype};
-
-    tmpName = strrep (tmpName,'all', '');
-    tmpName = strrep (tmpName,'_1_', ' 01 ');
-    tmpName = strrep (tmpName,'_7_', ' 07 ');
-    tmpName = strrep (tmpName,'_', ' ');
-    tmpName = strrep (tmpName,'D ', '');
-    tmpName = strrep (tmpName,'0uM Bottle', '');
-      
-    fittedNames{end + 1}= tmpName;
-   
-    fprintf('%s had %d flies \n', tmpName, analysisStruct.nFlies{thisPhenotype});
-    if ( analysisStruct.nFlies{thisPhenotype} > 1 ) 
-        fliesWithRealData = fliesWithRealData + 1 ;
-    end
-end
-
-fprintf('\n\n\n');
-%% sort names sanely
-[sortednames,sortIndex] = sort(fittedNames);
-for thisPhenotype=1:length(fittedNames)
-    fprintf('%s had %d flies \n', fittedNames{sortIndex(thisPhenotype)}, analysisStruct.nFlies{sortIndex(thisPhenotype)});
-end
-%% 
-
-%%Throw away singletons and photodiode
-iNext = 1 ;
-myRealBootData = [];
-for i=1:length(sortIndex)
-    if (sortIndex(i) < length(fittedNames))
-        if (analysisStruct.nFlies{sortIndex(i)} > 2)
-            if (~ strcmpi('Photodiode',sortednames(i)))
-                tmp = bootFitParams(sortIndex(i),:,:,:,:);
-                myRealBootData (iNext,:,:,:,:) = tmp ;
-                plotNames(iNext) = sortednames(i) ;
-                iNext = iNext + 1 ;
-            end
-        end
-    end
-end
-iNext = iNext - 1;
-
-%%
-xToPlot = linspace(0,iNext,iNext);
-if fliesWithRealData < 4 % try 4
+xToPlot = linspace(0,length(fittedNames),length(fittedNames));
+if length(fittedNames) < 4 % try 4
     sStyle = 'horizontal' ;
     xAngle = 0 ;
 else
@@ -75,32 +27,33 @@ else
     xAngle = 45 ;
 end
 
-
+for phenotypeIndex=1:length(fittedNames)
+    tmpName = fittedNames{phenotypeIndex};
+    tmpName = strrep (tmpName,'all', '');
+    tmpName = strrep (tmpName,'_', ' ');
+    fittedNames{phenotypeIndex}= tmpName;
+end
 
 %% Plot Rmax non parametric version...   
 % to get the scale the same in each case... , and allow for headroom multiply by 1.2
-% maxRMax = max(max(max(max(squeeze(bootFitParams(:,:,:,:,1)))))) ;
-% maxRMax = maxRMax * 1.2 ;
-
-for iFreqComponent =1:2
-    %  We can plot these in boxplots like this:
-    %figure;
-    maxRMax = max(max(max(max(squeeze(myRealBootData(:,:,iFreqComponent,:,1))))));
+    maxRMax = max(max(max(max(squeeze(bootFitParams(:,:,:,:,1)))))) ;
     maxRMax = maxRMax * 1.2 ;
-    for thisMaskCondition=1:2
-       % sPlot = subplot(2,1,thisMaskCondition);
-        d= myRealBootData(:,thisMaskCondition,iFreqComponent,:,1);
-        dataToBoxplot=squeeze(d)';
 
-        figure();
-        boxplot(dataToBoxplot,'plotstyle','compact','labelorientation',sStyle,'notch','on','labels', plotNames); % The Rmax fits for both mask and unmasked
+    for iFreqComponent =1:2
+    %  We can plot these in boxplots like this:
+    figure;
+    
+    for thisMaskCondition=1:2
+        sPlot = subplot(2,1,thisMaskCondition);
+        dataToBoxplot=squeeze(bootFitParams(:,thisMaskCondition,iFreqComponent,:,1))';
+        
+        boxplot(dataToBoxplot,'labelorientation',sStyle,'notch','on','labels', fittedNames); % The Rmax fits for both mask and unmasked
         %%If you have lots of data, you get a smaller plot...
         %boxplot(dataToBoxplot,'notch','on','plotstyle','compact','labels', fittedNames);
-        
         set(gca,'YLim', [0, maxRMax]);
     end
     sTmp = strcat('Rmax median unmasked and masked ', harmonicNames(iFreqComponent,1));
-    set(gcf,'Name',char(sTmp)); %% ] 1F1');
+    set(gcf,'Name',char(sTmp)); %% ] 1F1'); 
     %
     %notBoxPlot(dataToBoxplot); errorbar(mean(dataToBoxplot), std(dataToBoxplot));
 end ;
@@ -114,10 +67,10 @@ end ;
     
     for thisMaskCondition=1:2
         sPlot = subplot(2,1,thisMaskCondition);
-        dataToBoxplot=squeeze(myRealBootData(:,thisMaskCondition,iFreqComponent,:,1))';
+        dataToBoxplot=squeeze(bootFitParams(:,thisMaskCondition,iFreqComponent,:,1))';
         %notBoxPlot(dataToBoxplot); errorbar(mean(dataToBoxplot), std(dataToBoxplot));
         notBoxPlot(dataToBoxplot,xToPlot, 0.8) ;
-        set(gca,'XTickLabel',plotNames);
+        set(gca,'XTickLabel',fittedNames);
         rotateXLabels( gca, xAngle );
         set(gca,'YLim', [0, maxRMax]);
     end
@@ -129,7 +82,7 @@ end ;
 
 %% Do the same for c50
    % to get the scale the same in each case... , and allow for headroom multiply by 1.2
-    maxc50 = max(max(max(max(squeeze(myRealBootData(:,:,:,:,2)))))) ;
+    maxc50 = max(max(max(max(squeeze(bootFitParams(:,:,:,:,2)))))) ;
     maxc50 = maxc50 * 1.2 ;
 for iFreqComponent =1:2
     %  We can plot these in boxplots like this:
@@ -174,19 +127,19 @@ end ;
 %%
 % And we can look at the raw histograms like this:
 %% 
-% for iFreqComponent =1:2
-%     for iPhenotypes = 1:length(fittedNames)
-%         figure;
-%         histDataToPlot=squeeze(bootFitParams(iPhenotypes,:,iFreqComponent,:,1));
-%         hist(histDataToPlot',20);
-%         sTmp = strcat('Rmax hist -', harmonicNames(iFreqComponent,1) ,'- Phenotype: ', fittedNames{iPhenotypes})
-%         xlabel(sTmp);
-%         ylabel('Frequency');
-%         %set(gca,'XLim', [0, maxRMax]);
-%         legend({'Unmasked','Masked'});
-%         set(gcf,'Name',char(sTmp));
-%     end
-% end
+for iFreqComponent =1:2
+    for iPhenotypes = 1:length(fittedNames)
+        figure;
+        histDataToPlot=squeeze(bootFitParams(iPhenotypes,:,iFreqComponent,:,1));
+        hist(histDataToPlot',20);
+        sTmp = strcat('Rmax hist -', harmonicNames(iFreqComponent,1) ,'- Phenotype: ', fittedNames{iPhenotypes})
+        xlabel(sTmp);
+        ylabel('Frequency');
+        %set(gca,'XLim', [0, maxRMax]);
+        legend({'Unmasked','Masked'});
+        set(gcf,'Name',char(sTmp));
+    end
+end
 
 
 %% Finally, we can do real statistics on these data using ANOVAs. First for the RMax
