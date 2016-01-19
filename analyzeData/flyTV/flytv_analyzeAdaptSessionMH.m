db8aa1c361a67d9f0617ff54a219bf5096782eb9
Alex Wade <wade@biolpc22.york.ac.uk>
2016-01-19 15:30:12 +0000

    Committed correct version - now with SNR checking and ability to crop out some additional timeseries data ARW

--- analyzeData/flyTV/flytv_analyzeAdaptSessionMH.m
+++ analyzeData/flyTV/flytv_analyzeAdaptSessionMH.m
@@ -41,18 +41,18 @@ end % Next session
    
 %% We now have a cell array that is 
 % {nSegments} each with
-% nReps  x nConds * nTimePoints x nChannels x nSessions
+% nReps  x nConds x nTimePoints x nChannels x nSessions
 % segCatData{1} contains the adaptation data, segCatData{2} contains the
 % probes.
 % For now, I'm interersted in the adapt sections because we want to know,
 % firstly, if we're getting a direction reversal response.
 
 adaptData=segCatData{2};
-% The size of this is nCondDypes x nSessions x nTimePoints x nChannels x
-% nReps
+croppedData=adaptData(:,:,1:4000,:,:); % This crops out a subset of the timeseries. There are 1000 samples per second...
+
 
 % Average across reps
-meanAdaptData=squeeze(mean(adaptData,1));
+meanAdaptData=squeeze(mean(croppedData,1));
 % We now have data from nSessions x nChannels individual flies. Break this
 % into separate flies...
 [nConds,nTimePoints,nChannels,nSessions]=size(meanAdaptData)
@@ -60,19 +60,61 @@ meanAdaptData=squeeze(mean(adaptData,1));
 meanAdaptFlyData=meanAdaptData(1:nConditions,1:nTimePoints,:);
 
 % Now compute ffts on inidividual bins and average across those
-nSamplesPerBin=1000;
+nSamplesPerBin=1000; % This is a fact about the digitizer, it works at 1000hz
 nBins=nTimePoints/nSamplesPerBin;
 nFlies=nSessions*nChannels;
 binnedData=reshape(meanAdaptFlyData,[nConds,nSamplesPerBin,nBins,nFlies]);
 ftBinData=(fft(binnedData(:,:,2:nBins,:),[],2));
+% Aveage across bins 
 meanBinnedData=squeeze(mean(ftBinData,3));
+
+
+% Let's take a look at the 16Hz response for all flies in the first
+% condition.
+rawProbeResponse=squeeze(meanBinnedData(1,17,:));
+rawNoiseAroundProbe=squeeze(meanBinnedData(1,[15 16 18 19],:));
+rmsNoise=squeeze(sqrt(mean(abs(rawNoiseAroundProbe).^2)));
+
+snrProbe=abs(rawProbeResponse)./rmsNoise'; % The tick at the end flips the direction of the second vector to match the other one
+goodFlies=find(snrProbe>5)
+
+meanBinnedData=meanBinnedData(:,:,goodFlies);
+goodBinnedTS=binnedData(:,:,:,goodFlies);
+% Plot the mean (across flies) time series for the different conditions
+figure (98);
+for thisPlot=1:4 % POlot different conditions in different windows
+    subplot(4,1,thisPlot)
+    plot(squeeze(mean(goodBinnedTS(thisPlot,126:end,:,:),4)));
+end
+
 
 % Average across flies
 flyBin=squeeze((mean((meanBinnedData),3)));
 semBinnedData=squeeze(std((meanBinnedData),[],3))/sqrt(nFlies);
 figure(2);
 
-g=barweb(squeeze(abs(flyBin(:,2:20)))',squeeze(semBinnedData(:,2:20))',1,[],'Probe responses','Frequency','Amp',bone,[],{'NoAdapt','AdaptL','Ctrl','AdaptR'});
+g=barweb(squeeze(abs(flyBin(:,2:51)))',squeeze(semBinnedData(:,2:51))',1,[],'Probe responses','Frequency','Amp',bone,[],{'NoAdapt','AdaptL','Ctrl','AdaptR'});
 axis on
+
+% Whatever... now let's take a look at the first second after the
+% adaptation. 
+% We know that there's a big transient that happens on the adapt/probe
+% change.
+% This is over by about 125ms. 
+% How about we do our analysis on the time periods after that in just the
+% first second
+%%
+croppedTSDataNoTransient=squeeze(goodBinnedTS(:,1:end,3,:));
+% Do the FFT just on this. Now we have cropped out two full data cycles so
+% the peaks will be in different places.
+ftDataCropped=fft(croppedTSDataNoTransient,[],2);
+% Average across flies
+meanftDataCropped=squeeze(mean(ftDataCropped,3));
+semCroppedData=squeeze(std(ftDataCropped,[],3))/sqrt(nFlies);
+
+figure(80);
+g2=barweb(squeeze(abs(meanftDataCropped(:,2:29)))',squeeze(semCroppedData(:,2:29))',1,[],'Probe responses','Frequency','Amp',bone,[],{'NoAdapt','AdaptL','Ctrl','AdaptR'});
+
+
 
    
