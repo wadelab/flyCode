fd803dcb1dc209f57c491b3d93154992b3c162b1
biol75 <biol75@york.ac.uk>
2016-11-15 14:50:14 +0000

    basic R analysis

---
+++ analyzeData/fly_arduino/RGraphandAnova.R
@@ -0,0 +1,9 @@
+# put data in the clipboard from excel
+xx <- read.table(pipe("pbpaste"), sep="\t", header=T, na.strings=c(""))
+head (xx)
+attach(xx)
+library(ggplot2)
+ggplot(xx, aes(x=genotype,y=peak.peak,color=genotype)) + geom_point(shape=1)
+myANOVA= aov(peak.peak ~ genotype)
+summary(myANOVA)
+TukeyHSD(myANOVA)
