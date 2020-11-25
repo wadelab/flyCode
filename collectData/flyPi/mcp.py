c7cdfe5eb45b0eaadb54e97e806d62705b23a196
Chris Elliott <cje2@york.ac.uk>
2020-11-25 11:56:29 +0000

    store stimulus contrast in first row of data and fft

--- collectData/flyPi/mcp.py
+++ collectData/flyPi/mcp.py
@@ -94,7 +94,7 @@ def do_ADC_with_wait(i):
     clock_for_ADC = core.Clock()
     clock_for_ADC.reset(0.0)
     
-    for frame_count in range(n_rows):  # this is enough to still be in the stimulus
+    for frame_count in range(1, n_rows):  # this is enough to still be in the stimulus
        while 1000 * 1000 * clock_for_ADC.getTime() < sampling_times[frame_count] :
            pass
        sampling_values[frame_count, 0] = 1000 * 1000 * clock_for_ADC.getTime()
@@ -107,17 +107,17 @@ def do_ADC_with_wait(i):
 Date = datetime.today().strftime('%Y-%m-%d-%H-%M-%S')
 myQ = Queue()
 # create an array
-qty = 8 #  max types of stimulus
+qty = 7 #  max types of stimulus
 #sf = 0.2/0.4/0.8 gives 4/18/16 stripes
+#these ought not to be less than 1%, or they won't work as integers later
 cordinates = numpy.zeros((qty, 2), dtype=float)
 cordinates [0,0] = 0.8
-cordinates [1,0] = 0.005
+cordinates [1,0] = 0.01
 cordinates [2,0] = 0.4
 cordinates [3,0] = 0.1
 cordinates [4,0] = 0.2
 cordinates [5,0] = 0.07
 cordinates [6,0] = 1.0
-cordinates [7,0] = 0.01
 
 
 
@@ -127,7 +127,7 @@ expt_clock = core.Clock()
 frame_rpts = 750 # 600 gives us 8 seconds
 #pdb.set_trace()
 stim_per_rpt = 4
-n_rows= 2 * stim_per_rpt * frame_rpts #need 2x because we do two halves of the loop
+n_rows= 2 * stim_per_rpt * frame_rpts + 1 #need 2x because we do two halves of the loop; +1 to allow for stimuli in first row
 sampling_values = numpy.zeros(( n_rows, qty + 1), dtype=int)
 sampling_times = numpy.linspace(0, 1665 * float(n_rows), n_rows)
     
@@ -147,12 +147,15 @@ for i in range(qty):
 for one_process in processes:
     one_process.join()
     
-numpy.savetxt('myData.csv', sampling_values, delimiter=',', fmt='%i', newline='\n')
-os.rename("myData.csv", "myData" + Date + ".csv")
-    
+   
 expt_time = expt_clock.getTime()    
 print('Expt time was ' + str(expt_time))
 #print('Wait time was ' + str(t_real_start))
+#save the stimuli (as %) in first row of data
+sampling_values[0,1:] = 100 * numpy.transpose(cordinates[:,0])
+numpy.savetxt('myData.csv', sampling_values, delimiter=',', fmt='%i', newline='\n')
+os.rename("myData.csv", "myData" + Date + ".csv")
+ 
 
 # matplotlib graph the raw data
 plt.subplot(2, 2, 1)  # (rows, columns, panel number)
@@ -167,13 +170,18 @@ ff = numpy.zeros((lx, qty), dtype=float)
 for i in range(qty):
     ff[:, i] = abs(numpy.fft.rfft(sampling_values[:, i + 1])) / 1000.0
 fx = numpy.linspace(0, rate / 2, len(ff))
+#save the stimuli in first row of FFT
+ff[0] = 100 * numpy.transpose(cordinates[:,0])
 
 #plot the fft up to 60Hz
 plt.subplot(2, 2, 2)  # (rows, columns, panel number)
 plt.plot(fx[1:601], ff[1:601], linestyle='solid', marker='None')
 plt.xlabel('frequency (Hz)')
 
-pdb.set_trace()
+
+
+
+#pdb.set_trace() 
 #ff[75,:] nicely gives the response at 1F1, 7.5Hz (x10 scale factor)
 ff_2d = numpy.reshape(ff[75], (-1, qty))
 ff_2d_tr = numpy.transpose(ff_2d)
@@ -182,22 +190,23 @@ coords_with_data = numpy.append(cordinates, ff_2d_tr, axis=1)
 ff_2d = numpy.reshape(ff[150], (-1, qty))
 ff_2d_tr = numpy.transpose(ff_2d)
 coords_with_data = numpy.append(coords_with_data, ff_2d_tr, axis=1)
-sorted_coords_with_data[:, 0] = 100 * coords_with_data[:, 0]
-sorted_coords_with_data = sorted_coords_with_data[sorted_coords_with_data[:,0].argsort()] # sort by first (zero) column see https://stackoverflow.com/questions/2828059/sorting-arrays-in-numpy-by-column
+coords_with_data[:, 0] = 100 * coords_with_data[:, 0]
+coords_with_data = coords_with_data[coords_with_data[:,0].argsort()] # sort by first (zero) column see https://stackoverflow.com/questions/2828059/sorting-arrays-in-numpy-by-column
 
 #pdb.set_trace()
 plt.subplot(2, 2, 4)  # (rows, columns, panel number)
-plt.plot(sorted_coords_with_data[:, 0], sorted_coords_with_data[:, 2], 'go-', label ='1F1') #, green dots and solid line
-plt.plot(sorted_coords_with_data[:, 0], sorted_coords_with_data[:, 3], 'bo-', label ='2F1') #, blue  dots and solid line
-ymax = 1.2 * numpy.max(sorted_coords_with_data[:, 2:3])
-plt.xlim(0,110)
+plt.plot(coords_with_data[:, 0], coords_with_data[:, 2], 'go-', label ='1F1') #, green dots and solid line
+plt.plot(coords_with_data[:, 0], coords_with_data[:, 3], 'bo-', label ='2F1') #, blue  dots and solid line
+ymax = 1.2 * numpy.max(coords_with_data[:, 2:3])
+plt.xlim(0.8,110)
+plt.xscale('log')
 plt.ylim(0,ymax)
 plt.xlabel('contrast (%)')
 plt.legend()
 
 
-numpy.savetxt('myCoordinates.csv', coords_with_data, delimiter=',', newline='\n')
-os.rename("myCoordinates.csv", "myCoordinates" + Date + ".csv")
+#numpy.savetxt('myCoordinates.csv', coords_with_data, delimiter=',', newline='\n')
+#os.rename("myCoordinates.csv", "myCoordinates" + Date + ".csv")
 
 # merge x axis (frequency data) and y FFT data
 fall = numpy.insert(ff, 0, fx, axis=1)
