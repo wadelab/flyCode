65dce359966688dd07dab4c12751d6abfe0fd4fb
Chris Elliott <cje2@york.ac.uk>
2020-12-11 11:59:02 +0000

    time array of stimuli

--- collectData/flyPi/CRF.py
+++ collectData/flyPi/CRF.py
@@ -15,26 +15,29 @@ frame_rate = mywin.getActualFrameRate()
 n_rows= 60 * 10 # 10 sec at 60 frames/sec
 fliptimes = numpy.zeros(( n_rows + 1, 3), dtype=int)
 expt_clock = core.Clock()
+myStimuli = []
 
 for i in range(1):  
-    #generate some stimuli, f1 is a Numpy array
-    f1 = filters.makeGrating(256, 90, 3, 0.0, 'sin', 0.7) # res, ori=0.0,  # in degrees  cycles=1.0,  phase=0.0,  # in degrees  gratType="sin",  contr=1.0)
-    f2 = f1 * -1
+    #generate some stimuli, fHoriz and fVert is a Numpy array
+    fHoriz = filters.makeGrating(256, 90, 1.0, 0.0, 'sin', 0.7) # res, ori=0.0,  # in degrees  cycles=1.0,  phase=0.0,  # in degrees  gratType="sin",  contr=1.0)
+    fVert =  filters.makeGrating(256, 0, 1.0, 0.0, 'sin', 0.3) # res, ori=0.0,  # in degrees  cycles=1.0,  phase=0.0,  # in degrees  gratType="sin",  contr=1.0)
     
-    f3 = f1 + filters.makeGrating(256, 0, 0.5, 0.0, 'sin', 0.3) # res, ori=0.0,  # in degrees  cycles=1.0,  phase=0.0,  # in degrees  gratType="sin",  contr=1.0)
-    f4 = f3 * -1
-    grate = visual.GratingStim(win=mywin,  size=20,  tex= f3)
-    grate_inv = visual.GratingStim(win=mywin,  size=20,  tex= f4)
-   
-    frame_count = 0 # one frame every 1/60 sec, every 5 frames invert grating, every 4 frames invert mask
+    
+    # loop through 60 stimuli; every 6 frames invert grating, every 5 frames invert mask
+    for j in range(60):
+        if j % 12 < 6 :
+            myStimuli.append(visual.GratingStim(win=mywin,  size=20,  tex = fVert))
+        else :
+            myStimuli.append(visual.GratingStim(win=mywin,  size=20,  tex = -fVert))
+ 
+    # pdb.set_trace()
+    
+    frame_count = 0 # one frame every 1/60 sec, first show of grating takes my Pi 45 ms, then 1 ms for second show
     while frame_count < n_rows: 
         mywin.flip()
         fliptimes[frame_count,0] = 1000 * 1000 * expt_clock.getTime() 
-        if frame_count % 8 < 4:
-            grate.draw() #first draw ~45 ms, subsequent draws are ~1.2 ms
-        else:
-            grate_inv.draw()
-        fliptimes[frame_count,1] = 1000 * 1000* expt_clock.getTime()        
+        myStimuli[frame_count % 60].draw()
+        fliptimes[frame_count,1] = 1000 * 1000 * expt_clock.getTime()        
         frame_count += 1        
         # 
         
