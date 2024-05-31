bdb82e419c870e69d9ed21eafb0a6978261ddae4
AbigailStretch <159795860+AbigailStretch@users.noreply.github.com>
2024-05-31 14:51:27 +0100

    Update generalDataAnalysis_AS.m
    added more input directory lists and looking at photoreceptor responses

--- generalToolboxFunctions/generalDataAnalysis_AS.m
+++ generalToolboxFunctions/generalDataAnalysis_AS.m
@@ -28,7 +28,7 @@ clear all;
 close all;
 
 nBootstraps=1000;
-fToExamine=96; % Change this depending on which harmonic you want. Over 1 second it would be 12 (or 15) - those are the 1F. For 2F you can look at 96,120 cycle
+fToExamine=48; % Change this depending on which harmonic you want. Over 1 second it would be 12 (or 15) - those are the 1F. For 2F you can look at 96,120 cycle
 close all
 
 %dataDir='c:\Users\wade\Documents\flyData2022\orgData\';
@@ -40,6 +40,7 @@ dataDir='/Users/abbiestretch/Documents/PhD/Vision';
 
 %inputDirList={  'DJ1beta_1dpe' , 'DJ1beta_3dpe', 'DJ1beta_5dpe', 'DJ1beta_7dpe','DJ1beta_10dpe','DJ1beta_14dpe','DJ1beta_21dpe','DJ1beta_28dpe' };  % This is a list of directories where you have saved the data specific to each genotype. For these will depend on your project
 %inputDirList={  'DJ1alpha_1dpe' , 'DJ1alpha_3dpe', 'DJ1alpha_5dpe', 'DJ1alpha_7dpe','DJ1alpha_10dpe','DJ1alpha_14dpe','DJ1alpha_21dpe','DJ1alpha_28dpe' };
+%inputDirList={  'W1118CS_1dpe' , 'W1118CS_3dpe', 'W1118CS_5dpe', 'W1118CS_7dpe','W1118CS_10dpe','W1118CS_14dpe','W1118CS_21dpe','W1118CS_28dpe' };
 %inputDirList={'DJ1alpha_1dpe','DJ1beta_1dpe','W1118CS_1dpe'}
 %inputDirList={'DJ1alpha_3dpe','DJ1beta_3dpe','W1118CS_3dpe'}
 %inputDirList={'DJ1alpha_5dpe','DJ1beta_5dpe','W1118CS_5dpe'}
@@ -51,10 +52,11 @@ dataDir='/Users/abbiestretch/Documents/PhD/Vision';
 
 %inputDirList={  'Pink15_1dpe' , 'Pink15_3dpe', 'Pink15_5dpe', 'Pink15_7dpe','Pink15_10dpe','Pink15_14dpe','Pink15_21dpe' };
 %inputDirList={  'Pink1B9_1dpe' , 'Pink1B9_3dpe', 'Pink1B9_5dpe', 'Pink1B9_7dpe','Pink1B9_10dpe','Pink1B9_14dpe','Pink1B9_21dpe','Pink1B9_28dpe' };
+%inputDirList={  'W1118CSfem_1dpe' , 'W1118CSfem_3dpe', 'W1118CSfem_5dpe', 'W1118CSfem_7dpe','W1118CSfem_10dpe','W1118CSfem_14dpe','W1118CSfem_21dpe','W1118CSfem_28dpe' };
 %inputDirList={'Pink15_1dpe','Pink1B9_1dpe','W1118CSfem_1dpe'}
 %inputDirList={'Pink15_3dpe','Pink1B9_3dpe','W1118CSfem_3dpe'}
 %inputDirList={'Pink15_5dpe','Pink1B9_5dpe','W1118CSfem_5dpe'}
-%inputDirList={'Pink15_7dpe','Pink1B9_7dpe','W1118CSfem_7dpe'}
+inputDirList={'Pink15_7dpe','Pink1B9_7dpe','W1118CSfem_7dpe'}
 %inputDirList={'Pink15_10dpe','Pink1B9_10dpe','W1118CSfem_10dpe'}
 %inputDirList={'Pink15_14dpe','Pink1B9_14dpe','W1118CSfem_14dpe'}
 %inputDirList={'Pink15_21dpe','Pink1B9_21dpe','W1118CSfem_21dpe'}
